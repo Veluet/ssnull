82fa348acd6374acf6d41006ced6d037b531a2d8
Steven Soblo <steven.soblo@gmail.com>
2022-11-16 17:31:30 -0600

    Core CSS and Design Effort

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,143 @@
 <script>
+	import '../app.scss';
 	//import './styles.css';
 	import { page } from '$app/stores';
-	let currentPath
-	$: currentPath = $page.url.pathname.split('/').filter(x => x !== '');
+	let currentPath;
+	$: currentPath = $page.url.pathname.split('/').filter((x) => x !== '');
 	console.log(currentPath);
 </script>
 
 <div class="app">
-	<div class="content-wrapper">
-		<header>
-			{#if currentPath.length > 0}
-				<a href="/">
-					<h1>SSNULL</h1>
-				</a>
-			{:else}
-				<h1>SSNULL</h1>
-			{/if}
-		</header>
+	<div class="the-box-wrapper-1">
+		<div class="the-box-wrapper-2">
+			<div class="the-box">
+				<header>
+					{#if currentPath.length > 0}
+						<a href="/">
+							<h1>SSNULL</h1>
+						</a>
+					{:else}
+						<h1>SSNULL</h1>
+					{/if}
+				</header>
 
-		<main>
-			<slot />
-		</main>
+				<main>
+					<slot />
+				</main>
 
-		<footer />
+				<footer />
+			</div>
+		</div>
 	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.app {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background: #f0f0f0;
+		position: relative;
+		padding: 4em 4em 22em;
+		background-position: 0 100%;
+		background-image: url('/src/lib/images/bird.png');
+		background-repeat: no-repeat;
+		background-size: 930px auto;
+		min-height: 100%;
 	}
-	.content-wrapper {
+	.the-box {
+		position: relative;
+		z-index: 2;
 		border: 2px solid #000;
 		border-radius: 3px;
-		padding: 2em;
-		background: #fff;
+		background: rgba(255, 255, 255, 0.9);
+		padding: 1em;
+		min-height: 260px; // Prevents position issue with bird background.
+	}
+
+	// X-Small devices (portrait phones, less than 576px)
+	@media (max-width: 575.98px) {
+		.app {
+			background-size: 700px auto;
+			padding: 1.5em 1.5em 20em;
+		}
+		.the-box-wrapper-1:before,
+		.the-box-wrapper-1:after,
+		.the-box-wrapper-2:before,
+		.the-box-wrapper-2:after {
+			background-size: 50% !important;
+		}
+		.app {
+			.the-box-wrapper-1:before {
+				top: -17px;
+				left: -52px;
+			}
+			.the-box-wrapper-1:after {
+				top: -17px;
+				right: -14px;
+			}
+			.the-box-wrapper-2:before {
+				bottom: -45px;
+				left: -20px;
+			}
+			.the-box-wrapper-2:after {
+				bottom: -23px;
+				right: -31px;
+			}
+		}
+	}
+
+	// Now for the accents around the box
+	.the-box-wrapper-1,
+	.the-box-wrapper-2 {
+		position: relative;
+		width: 100%;
+	}
+	.box-accent {
+		content: '';
+		z-index: 1;
+		position: absolute;
+		background-repeat: no-repeat;
+		background-size: 100%;
+	}
+	.app {
+		--background-size: .75;
+	}
+
+	/* 
+		What makes this layout of this site insanity mode is these 4 absolute positioned
+		backgrounds that skirt each corner of the content frame.  The height on them is quite
+		large, which can push a height past <body>.
+	*/
+	.the-box-wrapper-1:before {
+		@extend .box-accent;
+		top: -25px;
+		left: -81px;
+		width: calc(426px * var(--background-size));
+		height: calc(795px *  var(--background-size));
+		background-image: url('/src/lib/images/top-left-scratches.png');
+	}
+	.the-box-wrapper-1:after {
+		@extend .box-accent;
+		top: -26px;
+		right: -35px;
+		width: calc(365px *  var(--background-size));
+		height: calc(644px *  var(--background-size));
+		background-image: url('/src/lib/images/top-right-scratches.png');
+		background-position: 100% 0;
+	}
+	.the-box-wrapper-2:before {
+		@extend .box-accent;
+		bottom: -65px;
+		left: -28px;
+		width: calc(497px *  var(--background-size));
+		height: calc(405px *  var(--background-size));
+		background-image: url('/src/lib/images/bottom-left-scratches.png');
+		background-position: 0 100%;
+	}
+	.the-box-wrapper-2:after {
+		@extend .box-accent;
+		bottom: -35px;
+		right: -49px;
+		width: calc(508px *  var(--background-size));
+		height: calc(272px *  var(--background-size));
+		background-image: url('/src/lib/images/bottom-right-scratches.png');
+		background-position: 100% 100%;
 	}
 </style>
--- src/routes/articles/product-and-design/signup-redesign/+page.svelte
+++ src/routes/articles/product-and-design/signup-redesign/+page.svelte
@@ -1,5 +1,61 @@
 <script>
-
 </script>
 
-<p>Yadda yadda made this pretty cool!</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+	labore et dolore magna aliqua. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut
+	placerat. Lobortis elementum nibh tellus molestie nunc. Augue interdum velit euismod in
+	pellentesque massa placerat. Consequat semper viverra nam libero justo. Tellus in metus vulputate
+	eu scelerisque felis imperdiet proin fermentum. Mattis enim ut tellus elementum sagittis vitae et.
+	Mi proin sed libero enim. Nam aliquam sem et tortor. Arcu dui vivamus arcu felis. Tempus urna et
+	pharetra pharetra massa massa ultricies. Aliquet risus feugiat in ante metus dictum. Adipiscing
+	enim eu turpis egestas pretium aenean pharetra magna ac. Odio pellentesque diam volutpat commodo
+	sed egestas egestas. Risus viverra adipiscing at in tellus integer feugiat scelerisque. Mattis
+	enim ut tellus elementum sagittis vitae et leo duis. Ac placerat vestibulum lectus mauris ultrices
+	eros. Consequat id porta nibh venenatis cras sed. Aliquet risus feugiat in ante metus.
+	Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit.
+</p>
+
+<p>
+	Sollicitudin tempor id eu nisl nunc mi. Sem integer vitae justo eget magna fermentum iaculis. Vel
+	turpis nunc eget lorem dolor. Justo laoreet sit amet cursus sit amet. Lobortis feugiat vivamus at
+	augue. Magna etiam tempor orci eu lobortis elementum nibh tellus. Ultricies lacus sed turpis
+	tincidunt. Etiam dignissim diam quis enim lobortis scelerisque fermentum. Posuere urna nec
+	tincidunt praesent. Sed turpis tincidunt id aliquet. Amet luctus venenatis lectus magna fringilla
+	urna porttitor. Malesuada fames ac turpis egestas maecenas pharetra convallis. Blandit turpis
+	cursus in hac habitasse platea dictumst quisque. Malesuada nunc vel risus commodo. Sem integer
+	vitae justo eget magna. Justo donec enim diam vulputate ut. Mattis vulputate enim nulla aliquet
+	porttitor. Id venenatis a condimentum vitae sapien pellentesque habitant morbi.
+</p>
+
+<p>
+	Placerat orci nulla pellentesque dignissim enim sit. Gravida in fermentum et sollicitudin ac orci
+	phasellus. Consectetur lorem donec massa sapien faucibus et. Cursus sit amet dictum sit amet justo
+	donec enim. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Felis eget
+	velit aliquet sagittis id consectetur purus ut faucibus. Nisl nisi scelerisque eu ultrices vitae
+	auctor eu augue ut. Amet justo donec enim diam. Id diam maecenas ultricies mi eget. Leo urna
+	molestie at elementum eu facilisis sed odio. Pellentesque dignissim enim sit amet venenatis urna
+	cursus. Egestas purus viverra accumsan in nisl nisi scelerisque. Amet nisl suscipit adipiscing
+	bibendum est ultricies integer. Sit amet cursus sit amet. Facilisi nullam vehicula ipsum a arcu
+	cursus vitae congue mauris. Vitae ultricies leo integer malesuada nunc vel.
+</p>
+
+<p>
+	Enim facilisis gravida neque convallis a cras semper. Et odio pellentesque diam volutpat commodo
+	sed egestas. Libero justo laoreet sit amet cursus sit. In hac habitasse platea dictumst quisque
+	sagittis purus sit amet. A scelerisque purus semper eget duis at. Volutpat est velit egestas dui.
+	Dictum sit amet justo donec enim diam. Venenatis lectus magna fringilla urna porttitor rhoncus
+	dolor purus non. Sed arcu non odio euismod lacinia at quis. Nam at lectus urna duis convallis
+	convallis tellus. Id ornare arcu odio ut sem nulla pharetra diam sit.
+</p>
+
+<p>
+	Eu consequat ac felis donec et odio. Commodo sed egestas egestas fringilla phasellus. Laoreet id
+	donec ultrices tincidunt arcu. Fusce id velit ut tortor pretium viverra. Ut tristique et egestas
+	quis ipsum suspendisse ultrices. At urna condimentum mattis pellentesque id nibh tortor id
+	aliquet. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Mauris
+	rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Ut ornare lectus sit amet. Diam
+	quam nulla porttitor massa id neque aliquam vestibulum morbi. Bibendum est ultricies integer quis
+	auctor elit sed. Sed viverra ipsum nunc aliquet bibendum. Urna molestie at elementum eu. At
+	volutpat diam ut venenatis. Non consectetur a erat nam.
+</p>
