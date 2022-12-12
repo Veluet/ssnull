0e5b1d743e4fc17f32e4a8b12475c4c6a9e60be2
Steven Soblo <steven.soblo@gmail.com>
2022-12-12 17:18:50 -0600

    Onboarding and Helper Loops articles.

--- src/routes/articles/frontend-code/helper-loops-for-sass/+page.svelte
+++ src/routes/articles/frontend-code/helper-loops-for-sass/+page.svelte
@@ -1,5 +1,319 @@
 <script>
-
+	import Prism from 'svelte-prism';
 </script>
 
-<p>Yadda yadda made this pretty cool!</p>
+<p>
+	I tend to find from project to project I need the same handful of helper classes. Whenever
+	possible I use mixins to generate these helpers due to the ease of maintenance&mdash;it's nice to
+	be able to add or remove additional classes to a range.
+</p>
+
+<h4>The Helper Loop</h4>
+
+<p>
+	This is the classic mixin I use for generating helper classes, which I just call the helper loop:
+</p>
+
+<Prism>
+	{`// e.g. .a-1 {z-index:1} .a-2 {z-index:2} .a-3 {z-index:3}
+@mixin helper-loop($start, $stop, $step, $class-name, $property, $unit, $unit-start: false,
+                    $unit-step: false, $extend: "") {
+
+    $i: $start;
+
+    @if not $unit-start {
+    $unit-start: $start;
+    }
+
+    @if not $unit-step {
+    $unit-step: $step;
+    }
+
+    $value: $unit-start;
+
+    @while $i <= $stop {
+
+    .#{$class-name}-#{$i} {
+        #{$property}: #{$value}#{$unit}!important;
+    };
+
+    @if ($extend != "") {
+        .#{$class-name}-#{$i} { @extend #{$extend} }
+    }
+
+    $value: $value + $unit-step;
+
+    $i: $i + $step;
+
+    }
+}`}
+</Prism>
+
+Let's take a look at an example for generating some margin classes. First is the more simple
+example, where <code>.margin-15</code> gives a value of <code>margin: 15px</code>:
+
+<Prism>
+	{`@include helper-loop(0, 15, 5, 'margin', 'margin', 'px');`}
+</Prism>
+
+Or maybe we want to abstract the numerical value on the helper class from the unit itself, so
+<code>.margin-4</code>
+to get <code>margin: 1rem</code>:
+
+<Prism>
+	{`@include helper-loop(0, 4, 1, 'margin', 'margin', 'rem', 0, .25);`}
+</Prism>
+
+With fonts maybe we want to skip the odd numbers:
+
+<Prism>
+	{`@include helper-loop(8, 36, 2, 'font-size', 'font-size', 'rem', 0.625, 0.125);`}
+</Prism>
+
+<p>
+	I like to have the number on the end of the helper class line up with the pixel size of a font, as
+	it's easier to visualize what the result will be. So in this case <code>font-size-12</code> might
+	be <code>1em</code>, but visually I want that to be the same as <code>12px</code>.
+</p>
+
+<p>
+	Sometimes I'll setup two loops next to each other. Maybe I want a few odd number font sizes in
+	that bread in butter range of 11&ndash;17 pixels:
+</p>
+
+<Prism>
+	{`@include helper-loop(8, 36, 2, 'font-size', 'font-size', 'rem', 0.625, 0.125);
+@include helper-loop(11, 17, 2, 'font-size', 'font-size', 'rem', 0.6875, 0.125);`}
+</Prism>
+
+<p>
+	I like to have some classes to nudge elements using <code>position: relative;</code>, but I really
+	just want to use a single helper class to do this like <code>.top-1</code>. This is where that
+	<code>extend</code> parameter comes in:
+</p>
+
+<Prism>
+	{`.relative {
+    position: relative;
+}
+  
+@include helper-loop(1, 5, 1, 'top', 'top', 'px', false, false, ".relative");`}
+</Prism>
+
+<p>
+	Then all I need is one class to nudge an element <span class="bottom-2">up</span> or
+	<span class="bottom-2" />. I love this one for icons which rarely have the same visual baseline as
+	text:
+</p>
+
+<Prism language="html">
+	{`Then all I need is one class to nudge an element <span class="bottom-2">up</span> or <span class="bottom-2"></span>`}
+</Prism>
+
+<h4>Responsive Classes</h4>
+
+<p>
+	Building out responsive classes is more complex. I use a loop in a loop to do this. I also don't
+	use a global mixin for this like the other helpers, preferring to copy, paste and modify an
+	existing loop.
+</p>
+
+<p>
+	I'll use one of my spacing loops to demonstrate. Let's start with just one helper class to
+	understand what we are creating:
+</p>
+
+<Prism language="css">
+	{`
+    .col-margin-1 > * {
+        margin-left: .25rem;
+        margin-right: .25rem;
+    }
+    .col-margin-1 > *:first-child {
+        margin-left: 0;
+    }
+    .col-margin-1 > *:last-child {
+        margin-right: 0;
+    }
+`}
+</Prism>
+
+<p>
+	Having child selectors and psuedo selectors complicates this enough that I can't use my helper
+	loop mixin, and responsive classes further complicate matters. First thing I need is a map of my
+	breakpoints. I like to match my breakpoints to what Bootstrap uses as they're easy to commit to
+	memory:
+</p>
+
+<Prism>
+	{`$bootstrap-breakpoints: (
+    'xs': 0,
+    'sm': 576px,
+    'md': 768px,
+    'lg': 992px,
+    'xl': 1200px,
+    'xxl': 1400px,
+);`}
+</Prism>
+
+<p>
+	I want to be able to pass parameters into a function of some sort and have that spit out all the
+	helper classes for me. That function call will look like this:
+</p>
+
+<Prism>{`@include spacing-loop(.25, 4, 'margin');`}</Prism>
+
+<p>
+	I'm going to be calling this loop a couple of times to generate <code>.col-margin-8</code>,
+	<code>.col-margin-12</code>
+	and <code>.col-margin-16</code>. So there is a hidden parameter of
+	<code>$spacing-breakpoints</code> which is map of all the breakpoints I want to generate classes for.
+</p>
+
+<Prism>
+	{`$spacing-breakpoints: 'xs', 'sm'; // 'md', 'lg', 'xl', 'xxl';`}
+</Prism>
+
+<p>Next we need to generate each of the media breakpoints:</p>
+
+<Prism>
+	{`@mixin spacing-breakpoint-loop($property, $i, $value, $breakpoints) {
+
+    @each $breakpoint in $breakpoints {
+
+    @media (min-width: map-get($bootstrap-breakpoints, $breakpoint)) {
+        @include spacing-styles($property, $i, $value, $breakpoint);
+    }
+
+    }
+
+}`}
+</Prism>
+
+<p>This means our last mixin has the actual classes and instructions in it:</p>
+
+<Prism>
+	{`@mixin spacing-styles($property, $i, $value, $breakpoint) {
+
+@if $breakpoint == 'xs' {
+  $breakpoint: '';
+}
+@else {
+  $breakpoint: '-#{$breakpoint}';
+}
+
+.col-#{$property}#{$breakpoint}-#{$i} > * {
+  #{$property}-left: calc(#{$value}rem / 2);
+  #{$property}-right: calc(#{$value}rem / 2);
+}
+.col-#{$property}#{$breakpoint}-#{$i} > *:first-child {
+  #{$property}-left: 0
+}
+.col-#{$property}#{$breakpoint}-#{$i} > *:last-child {
+  #{$property}-right: 0
+}
+}`}
+</Prism>
+
+<p>
+	The only thing I want to call out here is the check for the smallest possible breakpoint of <code
+		>'xs'</code
+	>. The media query applies to any screen size which 0px and above, which means that class applies
+	to all screen sizes and we don't need to designate those classes as <code>'-xs-'</code>.
+</p>
+
+<p>To close this out I'm going to include the entire <code>_spacing.scss</code> file.  Keep in mind that I have <code>$bootstrap-breakpoints</code> as global variable for all my <code>scss</code> files, but I'll include here for simplicity:</p>
+
+<Prism>
+{`/* Child Spacing
+* Stuff to generate a helper classes for setting the spacing on all child elements.
+*/
+
+/* EXAMPLE: .col-margin-1 or .row-margin-1 or .row-padding-1 or .col-margin-12 */
+// You get 1, 2, 3, 4, 8, 12, 16.  Padding isn't enabled (so enable if needed). Jump to end of file.
+
+// This should be a global variable available to all scss files.
+$bootstrap-breakpoints: (
+'xs': 0,
+'sm': 576px,
+'md': 768px,
+'lg': 992px,
+'xl': 1200px,
+'xxl': 1400px,
+);
+
+// Add the breakpoints as they are needed: 'xs', 'sm', 'md', 'lg', 'xl', 'xxl'
+$spacing-breakpoints: 'xs'; //, 'sm', 'md', 'lg', 'xl', 'xxl';
+
+@mixin spacing-styles($property, $i, $value, $breakpoint) {
+
+    @if $breakpoint == 'xs' {
+    $breakpoint: '';
+    }
+    @else {
+    $breakpoint: '-#{$breakpoint}';
+    }
+
+    .col-#{$property}#{$breakpoint}-#{$i} > * {
+    #{$property}-left: calc(#{$value}rem / 2);
+    #{$property}-right: calc(#{$value}rem / 2);
+    }
+    .col-#{$property}#{$breakpoint}-#{$i} > *:first-child {
+    #{$property}-left: 0
+    }
+    .col-#{$property}#{$breakpoint}-#{$i} > *:last-child {
+    #{$property}-right: 0
+    }
+
+    .row-#{$property}#{$breakpoint}-#{$i} > * {
+    #{$property}-top: calc(#{$value}rem / 2);
+    #{$property}-bottom: calc(#{$value}rem / 2);
+    }
+    .row-#{$property}#{$breakpoint}-#{$i} > *:first-child {
+    #{$property}-top: 0
+    }
+    .row-#{$property}#{$breakpoint}-#{$i} > *:last-child {
+    #{$property}-bottom: 0
+    }
+}
+
+@mixin spacing-breakpoint-loop($property, $i, $value, $breakpoints) {
+
+    @each $breakpoint in $breakpoints {
+
+    @media (min-width: map-get($bootstrap-breakpoints, $breakpoint)) {
+        @include spacing-styles($property, $i, $value, $breakpoint);
+    }
+
+    }
+
+}
+
+@mixin spacing-loop($amount, $steps, $property, $start: false) {
+
+    $i: 1;
+
+    @if $start {
+    $i: $start;
+    }
+
+    $value: $amount;
+
+    @while $i <= $steps {
+
+    @include spacing-breakpoint-loop($property, $i, $value, $spacing-breakpoints);
+
+    $value: $value + $amount;
+
+    $i: $i + 1;
+    }
+}
+// Example: .col-margin-1
+@include spacing-loop(.25, 4, 'margin');
+@include spacing-loop(2, 8, 'margin', 8); // For 2rem
+@include spacing-loop(3, 12, 'margin', 12); // For 3rem
+@include spacing-loop(4, 16, 'margin', 16); // For 4rem
+
+// Enable if needed.
+// @include spacing-loop(.25, 4, 'padding');`}
+</Prism>
--- src/routes/articles/product-and-design/onboarding-dashboard/+page.svelte
+++ src/routes/articles/product-and-design/onboarding-dashboard/+page.svelte
@@ -1,5 +1,106 @@
 <script>
+	import Carousel from './../../../Carousel.svelte';
+	import Image from './../../../Image.svelte';
 
+	const imagePath = '/images/articles/onboarding-dashboard/';
+
+	let dashboardDesigns = [
+		{
+			src: imagePath + 'dashboard-0-and-0.png',
+			alt: '',
+			summary:
+				'The dashboard on the first visit, before any configuration has been done on the account.'
+		},
+		{
+			src: imagePath + 'dashboard-1-and-0.png',
+			alt: '',
+			summary: 'The dashboard after "topics" have been configured.'
+		},
+		{
+			src: imagePath + 'dashboard-1-and-1.png',
+			alt: '',
+			summary: 'The dashboard after the account have been setup.'
+		}
+	];
 </script>
 
-<p>Yadda yadda made this pretty cool!</p>
+<Carousel images={dashboardDesigns} />
+
+<p>
+	I created the following designs for the introduction of a dashboard to the client's web
+	application. This was part of an ongoing effort to improve conversion rates for new customers, the
+	first piece was a <a href="/articles/product-and-design/signup-redesign">sign up redesign</a>.
+</p>
+
+<p>
+	The idea was to give users a starting point for when they enter application. Without the dashboard
+	users would log in and be presented the last screen they had visited. In cases where the account
+	wasn't setup, users were shown a welcome screen which directed users to either start with sample
+	content or figure it out themselves.
+</p>
+
+<h4>Stage 1: A New "Welcome" Screen</h4>
+
+<Image image={imagePath + 'dashboard-0-and-0.png'} />
+
+<p>
+	This product's main purpose is to find articles from search terms for sharing. The user creates a
+	query, which can have subqueries, to find articles. They can then further filter those results.
+	The last piece is sharing those results, which can be done with in-app or third party
+	integrations.
+</p>
+
+<p>
+	Before anything can be done in the product, the user needs to create a search. In the context of
+	this product, that is referred to as a topic query and is usually shorted to just <em>topic</em>.
+	Every customer will want to create at least one topic, and so the initial state of the dashboard
+	is focused just on that.
+</p>
+
+<p>
+	Creating a topic query had a simple first step, enter the name of the topic you want to do a
+	search on. But, if you've ever written a school paper then you've probably experience the
+	sensation of brain freeze. A world of infinite possibilities beyond an empty box for customers to
+	get stuck on. To solve for this writer's block scenario an inspiration section was included.
+</p>
+
+<p>
+	The pre-built topics are taken from a compiled list of the most common topics users were creating.
+</p>
+
+<h4>Stage 2: The "Now What" Step</h4>
+<Image image={imagePath + 'dashboard-1-and-0.png'} />
+<p>
+	Once user's have their first topic, it becomes very difficult for us to predict what they will
+	need to do next. Most users would need to create a "collection" at some point.
+</p>
+
+<p>
+	A collection was like a folder of results. Users could either manually send results to a
+	collection, or have a collection pull in results from another source automatically. A collection
+	could pull from multiple sources, which could be any combination of topic queries, other
+	collections or RSS feeds. When a collection pulls in sources, it can apply a filter as well. Most
+	of the third party integrations were applied to a collection in some way.
+</p>
+
+<p>
+	However, without these dashboard, none of this collection business would be visible to the users.
+	They had to learn it themselves, either by randomly clicking through the UI, reading help
+	articles, or contacting support.
+</p>
+
+<p>Integrations suffered from the same problem of being invisible to the user to an even greater extent.  Data was showing trial users who didn't convert never explored integrations.</p>
+
+<p>So the goal of this dashboard view was to expose the breadth of the product.  It still needed to guide users through setup, but needed to have a bit of a choose your own adventure angle to it.</p>
+
+<h4>Stage 3: Just a Normal Dashboard</h4>
+
+<Image image={imagePath + 'dashboard-1-and-1.png'} />
+
+<p>Dashboards are usually the big #2 in any web application, second only to the login page.  They're the gateway into application.  When you walk into the kitchen and pause for a brief moment to ask yourself, "why did I come in here again?" Bam, that's what this Dashboard hopes to solve.</p>
+
+<p>I didn't want to wildly disrupt the current behavior of dropping the user back to the last place they were in the app.  Thus the decision to have a list of the most recently visited topics and collections.  This also allowed for showing multiple results and evolved into being able to pin favorites.</p>
+
+<p>To address some additional concerns the dashboard can also be further tuned by the user.  This includes the ability to hide the large "Create a new..." sections and change the max number of recent items to show.</p>
+
+<p>Having a dashboard also created real estate to expose additional features of the product.  For these initial designs, that was to give visibility to third party integrations.</p>
